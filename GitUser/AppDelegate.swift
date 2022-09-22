382cabc70497ecbbbb2cef7b83fd63b495bfcd0f
jan <jnsbstn391@gmail.com>
2022-09-22 19:21:06 +0700

    - lock device orientation to potrait

--- GitUser/AppDelegate.swift
+++ GitUser/AppDelegate.swift
@@ -26,6 +26,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return .portrait
+    }
     
 
 
